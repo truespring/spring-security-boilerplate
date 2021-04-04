27bf4f018959823f887d905a4425beb5fdb8f01f
David <zkffhtm6523@naver.com>
2021-04-04 15:07:29 +0900

    Spring Security 주석 처리 제거

--- src/main/java/com/mini2S/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/mini2S/configuration/WebSecurityConfiguration.java
@@ -1,56 +1,57 @@
-//package com.mini2S.board.configuration;
+package com.mini2S.configuration;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception{
-//        http
-//                .authorizeRequests()
-//                    // 아래의 경로는 권한 허용
-//                    .antMatchers("/", "/css/**", "/js/**").permitAll()
-//                    .anyRequest().authenticated()
-//                    .and()
-//                .formLogin()
-//                    // 로그인 페이지도 권한 허용
-//                    .loginPage("/login")
-//                    .permitAll()
-//                    .and()
-//                .logout()
-//                    .permitAll();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .passwordEncoder(passwordEncoder())
-//                .usersByUsernameQuery("select username, password, enabled "
-//                                    + "from user "
-//                                    + "where username =?")
-//                .authoritiesByUsernameQuery("select username, name "
-//                                    + "from user_role ur inner join user u on ur.user_id = u.id "
-//                                    + "inner join role r on ur.role_id = r.id "
-//                                    + "where email = ?");
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                    // 아래의 경로는 권한 허용
+                    .antMatchers("/", "/css/**", "/js/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    // 로그인 페이지도 권한 허용
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username, password, enabled "
+                                    + "from user "
+                                    + "where username =?")
+                .authoritiesByUsernameQuery("select username, name "
+                                    + "from user_role ur inner join user u on ur.user_id = u.id "
+                                    + "inner join role r on ur.role_id = r.id "
+                                    + "where email = ?");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
+
