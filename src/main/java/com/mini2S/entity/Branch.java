1ca70eb868cb696e3e8731c924fb657a6333bf20
David <zkffhtm6523@naver.com>
2021-04-09 21:43:33 +0900

    향후 Entity를 정의할 테이블 명세서 리스트 업

---
+++ src/main/java/com/mini2S/entity/Branch.java
@@ -0,0 +1,33 @@
+package com.mini2S.entity;
+
+public class Branch {
+    /*
+        branch_id
+        branch_name
+        branch_code
+        zipcode
+        address1
+        address2
+        coord_x
+        coord_y
+        USE_YN
+        OPEN_DT
+        CONTACT
+        SIDO
+        SIGUNGU
+        BNAME
+        NAVI
+        BUS
+        SUBWAY
+        PRIORITY
+        PIC01
+        PIC02
+        PIC03
+        PIC04
+        PIC05
+        TEMP
+        HUMIDITY
+
+
+    * */
+}
--- src/main/java/com/mini2S/entity/Units.java
+++ src/main/java/com/mini2S/entity/Units.java
@@ -1,4 +1,27 @@
 package com.mini2S.entity;
 
 public class Units {
+    /*
+        룸ID	ROOM_ID
+        지점ID	BRANCH_ID
+        상품ID	PRODUCT_ID
+        룸코드	ROOM_CD
+        룸명	ROOM_NAME
+        폭	SIZE_WIDTH
+        높이	SIZE_HEIGHT
+        깊이	SIZE_DEPTH
+        상태	STATUS
+        가격	PRICE
+        비고	NOTE
+        부피	SIZE_VOLUME
+        넓이	SIZE_AREA
+        기둥 폭	COLUMN_WIDTH
+        기둥 높이	COLUMN_HEIGHT
+        기둥 깊이	COLUMN_DEPTH
+        기둥 부피	COLUMN_VOLUME
+        기둥 넓이	COLUMN_AREA
+        실제 부피	ACTUAL_VOLUME
+        실제 넓이	ACTUAL_AREA
+        평	PY
+    */
 }
