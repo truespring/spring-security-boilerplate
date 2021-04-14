9ee037d9c897725bc922d1937e444062e2f9007c
David <zkffhtm6523@naver.com>
2021-04-14 22:58:02 +0900

    테이블 설명 업데이트 중

--- src/main/java/com/mini2S/entity/Branch.java
+++ src/main/java/com/mini2S/entity/Branch.java
@@ -2,32 +2,34 @@ package com.mini2S.entity;
 
 public class Branch {
     /*
-        branch_id
-        branch_name
-        branch_code
-        zipcode
-        address1
-        address2
-        coord_x
-        coord_y
-        USE_YN
-        OPEN_DT
-        CONTACT
-        SIDO
-        SIGUNGU
-        BNAME
-        NAVI
-        BUS
-        SUBWAY
-        PRIORITY
-        PIC01
-        PIC02
-        PIC03
-        PIC04
-        PIC05
-        TEMP
-        HUMIDITY
-
-
+SEQ	PK	FK	컬럼설명	컬럼명	    데이터타입	길이	NULL	기본값	참조	설명
+1	O		지점ID	BRANCH_ID	VARCHAR	    100	X
+2			지점명	BRANCH_NAME	VARCHAR	    100	X
+3			지점코드	BRANCH_CODE	VARCHAR	    100	X
+4			우편번호	ZIPCODE	    VARCHAR	    16
+5			주소	    ADDRESS1	VARCHAR	    255
+6			상세주소	ADDRESS2	VARCHAR	    255
+7			좌표x	COORD_X	    DECIMAL	    20,10
+8			좌표y	COORD_Y	    DECIMAL	    20,10
+9			전화번호	PHONE	    VARCHAR	    100
+10			팩스번호	FAX	        VARCHAR	    100
+11			이메일	EMAIL	    VARCHAR	    255
+12			사용여부	USE_YN	    MEDIUMINT	9				Y : 활성, N : 비활성
+13			오픈일	OPEN_DT	    TIMESTAMP			'0000-00-00 00:00:00'
+14			지점대표번호	CONTACT	VARCHAR	100
+15			시도	    SIDO	VARCHAR	100
+16			시군구	SIGUNGU	VARCHAR	100
+17			법정동/법정리	BNAME	VARCHAR	100
+18			네비게이션	NAVI	VARCHAR	255
+19			버스	    BUS	VARCHAR	255
+20			지하철	SUBWAY	VARCHAR	255
+21			우선순위	PRIORITY	INT	11	X
+22		O	지점사진ID 1	PIC01	VARCHAR 	100			T_FILE.FILE_ID
+23		O	지점사진ID 2	PIC02	VARCHAR	    100			T_FILE.FILE_ID
+24		O	지점사진ID 3	PIC03	VARCHAR	    100			T_FILE.FILE_ID
+25		O	지점사진ID 4	PIC04	VARCHAR 	100			T_FILE.FILE_ID
+26		O	지점사진ID 5	PIC05	VARCHAR 	100			T_FILE.FILE_ID
+27			온도      	TEMP	VARCHAR	7
+28			습도	        HUMIDITY	VARCHAR	7
     * */
 }
--- src/main/java/com/mini2S/entity/Units.java
+++ src/main/java/com/mini2S/entity/Units.java
@@ -2,26 +2,27 @@ package com.mini2S.entity;
 
 public class Units {
     /*
-        룸ID	ROOM_ID
-        지점ID	BRANCH_ID
-        상품ID	PRODUCT_ID
-        룸코드	ROOM_CD
-        룸명	ROOM_NAME
-        폭	SIZE_WIDTH
-        높이	SIZE_HEIGHT
-        깊이	SIZE_DEPTH
-        상태	STATUS
-        가격	PRICE
-        비고	NOTE
-        부피	SIZE_VOLUME
-        넓이	SIZE_AREA
-        기둥 폭	COLUMN_WIDTH
-        기둥 높이	COLUMN_HEIGHT
-        기둥 깊이	COLUMN_DEPTH
-        기둥 부피	COLUMN_VOLUME
-        기둥 넓이	COLUMN_AREA
-        실제 부피	ACTUAL_VOLUME
-        실제 넓이	ACTUAL_AREA
-        평	PY
+SEQ	PK	FK	컬럼설명	   컬럼명	    데이터타입	길이	NULL
+1	O		룸ID	  ROOM_ID	    VARCHAR	100	X
+2		O	지점ID	  BRANCH_ID	    VARCHAR	100	X
+3		O	상품ID	  PRODUCT_ID	VARCHAR	100	X
+4			룸코드	  ROOM_CD	    VARCHAR	100	X
+5			룸명	      ROOM_NAME	    VARCHAR	100	X
+6			폭	      SIZE_WIDTH	MEDIUMINT	10	X
+7			높이	      SIZE_HEIGHT	MEDIUMINT	10	X
+8			깊이	      SIZE_DEPTH	MEDIUMINT	10	X
+9			상태	      STATUS	    VARCHAR	10	X
+10			가격       PRICE	        MEDIUMINT	9	X
+11			비고       NOTE	        VARCHAR	400
+12			부피	      SIZE_VOLUME	DECIMAL	10,5	X
+13			넓이	      SIZE_AREA	    DECIMAL	10,5	X
+14			기둥 폭	  COLUMN_WIDTH	MEDIUMINT	9	X
+15			기둥 높이   COLUMN_HEIGHT MEDIUMINT	9	X
+16			기둥 깊이   COLUMN_DEPTH	MEDIUMINT	9	X
+17			기둥 부피   COLUMN_VOLUME DECIMAL	10,5	X
+18			기둥 넓이   COLUMN_AREA	DECIMAL	10,5	X
+19			실제 부피   ACTUAL_VOLUME DECIMAL	10,5	X
+20			실제 넓이   ACTUAL_AREA	DECIMAL	10,5	X
+21			평	       PY	        DECIMAL	4,2
     */
 }
