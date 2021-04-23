7e7c2536a5e366b55662ece441ffc2569e5558cd
truespring <true_spring_@naver.com>
2021-04-23 15:37:14 +0900

    SessionDisconnect 테이블 생성 완료

--- src/main/java/com/mini2S/entity/SessionDisconnect.java
+++ src/main/java/com/mini2S/entity/SessionDisconnect.java
@@ -1,5 +1,16 @@
 package com.mini2S.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+@Setter
+@Entity
 public class SessionDisconnect {
 /*
 SEQ	PK	FK	컬럼설명	컬럼명	데이터타입	길이	NULL	기본값	참조	설명
@@ -12,4 +23,23 @@ SEQ	PK	FK	컬럼설명	컬럼명	데이터타입	길이	NULL	기본값	참조	�
 7			클라이언트 정보	CLIENT_INFO	VARCHAR	100
 8			자동로그인여부	AUTO_LOGIN	VARCHAR	20				Y : 자동로그인 N : 일반로그인
 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SERVICE_SESSION_SEQ")
+    private Long serviceSessionSeq;
+
+    @Column(columnDefinition = "varchar(100) not null comment '서버'")
+    private String serverSeq;
+
+    @Column(columnDefinition = "bigint not null comment '사용자'")
+    private Long userSeq;
+
+    @Column(columnDefinition = "varchar(400) comment 'UserAgent'")
+    private String userAgent;
+
+    @Column(columnDefinition = "varchar(100) comment '클라이언트 정보'")
+    private String clientInfo;
+
+    @Column(columnDefinition = "varchar(100) comment 'Y : 자동로그인 N : 일반로그인'")
+    private String autoLogin;
 }
