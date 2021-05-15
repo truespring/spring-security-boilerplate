e038cb63d3eb59c9362e3b6e4b5ea8387fcb1e85
David <zkffhtm6523@naver.com>
2021-05-15 23:44:05 +0900

    Unit 생성중

--- src/main/java/com/mini2S/biz/unit/controller/RestUnitController.java
+++ src/main/java/com/mini2S/biz/unit/controller/RestUnitController.java
@@ -1,21 +1,25 @@
 package com.mini2S.biz.unit.controller;
 
+import com.mini2S.biz.unit.model.entity.Unit;
+import com.mini2S.configuration.reposotory.UnitRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(tags = {"4. Unit"})
 @RequestMapping("/v1")
 @RestController
 @AllArgsConstructor
 public class RestUnitController {
+
+    private final UnitRepository unitRepository;
 
     @ApiImplicitParams({
             @ApiImplicitParam(
@@ -28,11 +32,7 @@ public class RestUnitController {
     })
     @GetMapping("/unit/list")
     @ApiOperation(value = "지점에 해당하는 유닛 리스트 전달")
-    public String unitList(String branchId){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        System.out.println("이메일 : "+email);
-        System.out.println("지점 아이디 : "+branchId);
-        return "";
+    public List<Unit> unitList(String branchSeq){
+        return unitRepository.findByBranchSeq(branchSeq);
     }
 }
---
+++ src/main/java/com/mini2S/configuration/reposotory/UnitRepository.java
@@ -0,0 +1,17 @@
+package com.mini2S.configuration.reposotory;
+
+import com.mini2S.biz.unit.model.entity.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UnitRepository extends JpaRepository<Unit, Long> {
+
+    @Query(value = " SELECT a.* " +
+            " FROM unit a " +
+            " WHERE a.branch_seq = :branchSeq "
+            , nativeQuery = true)
+    List<Unit> findByBranchSeq(@Param("branchSeq") String branchSeq);
+}
