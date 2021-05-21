8c0229b9c91edbc6fef57866de659997a10d90d4
David <zkffhtm6523@naver.com>
2021-05-21 21:52:30 +0900

    Contract에 QR 모듈 추가

--- src/main/java/com/mini2S/biz/contract/service/ContractService.java
+++ src/main/java/com/mini2S/biz/contract/service/ContractService.java
@@ -3,6 +3,8 @@ package com.mini2S.biz.contract.service;
 import com.mini2S.biz.contract.model.dto.InsertContractDto;
 import com.mini2S.model.response.CommonResult;
 
+import java.io.IOException;
+
 public interface ContractService {
-    CommonResult insertContract(InsertContractDto dto, String featureDirectory);
+    CommonResult insertContract(InsertContractDto dto, String featureDirectory) throws IOException;
 }
--- src/main/java/com/mini2S/biz/contract/service/ContractServiceImpl.java
+++ src/main/java/com/mini2S/biz/contract/service/ContractServiceImpl.java
@@ -2,11 +2,14 @@ package com.mini2S.biz.contract.service;
 
 import com.mini2S.biz.contract.model.dto.InsertContractDto;
 import com.mini2S.biz.contract.model.entity.Contract;
+import com.mini2S.common.util.qrcode.QrCode;
 import com.mini2S.configuration.reposotory.ContractRepository;
 import com.mini2S.model.response.CommonResult;
 import com.mini2S.service.ResponseService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @AllArgsConstructor
 @Service
@@ -16,7 +19,9 @@ public class ContractServiceImpl implements ContractService{
     private final ResponseService responseService;
 
     @Override
-    public CommonResult insertContract(InsertContractDto dto, String featureDirectory) {
+    public CommonResult insertContract(InsertContractDto dto, String featureDirectory) throws IOException {
+        String qrCodeImage = QrCode.createQRCodeImage(featureDirectory, String.valueOf(dto.getUserSeq())
+                                                , dto.getUserSeq() +"qrImage", "");
         contractRepository.save(dto.toEntity());
         return responseService.getSuccessResult();
     }
